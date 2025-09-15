e40b8c782d621a09f6c68c47724614d7bc46e5c5
Ashutosh Trivedi <ashutoshtrivedi9632@gmail.com>
2025-09-15 19:47:37 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { CartProvider } from '@/context/CartContext';
+import 'react-medium-image-zoom/dist/styles.css'; // Add this line for zoom styles
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
