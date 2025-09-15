ca20af73907f68c5b36c7772bd60ad00527ee767
Ashutosh Trivedi <ashutoshtrivedi9632@gmail.com>
2025-09-15 21:37:43 +0530

    Update layout.js

--- src/compoment/layout.js
+++ src/compoment/layout.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import { Header } from "./header";
 import { Footer } from "./footer";
+import styled from "styled-components";
+
+const MainContent = styled.main`
+  padding-top: 80px; 
+`;
 
 export const Layout = ({ children, headerData }) => {
   return (
@@ -10,7 +15,9 @@ export const Layout = ({ children, headerData }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Header headerData={headerData} />
-      <div>{children}</div>
+      <MainContent>
+        {children}
+      </MainContent>
       <Footer />
     </div>
   );
